cebeb5f26cc04744117783698d4bcc9de5a7dafe
Faraz Fallahi <fffaraz@gmail.com>
2016-07-15 11:32:04 -0500

    ttl

--- microdns.go
+++ microdns.go
@@ -13,10 +13,12 @@ import (
 )
 
 var ipv4, ipv6 string
+var ttl int
 
 func main() {
 	flag.StringVar(&ipv4, "ipv4", "127.0.0.1", "IPv4 Address")
 	flag.StringVar(&ipv6, "ipv6", "::1", "IPv6 Address")
+	flag.IntVar(&ttl, "ttl", 86400, "Time to live")
 	flag.Parse()
 	dns.HandleFunc(".", handleRequest)
 	go func() {
@@ -50,10 +52,10 @@ func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 	m.SetReply(r)
 	m.Authoritative = true
 	rr1 := new(dns.A)
-	rr1.Hdr = dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 86400}
+	rr1.Hdr = dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: uint32(ttl)}
 	rr1.A = net.ParseIP(ipv4)
 	rr2 := new(dns.AAAA)
-	rr2.Hdr = dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 86400}
+	rr2.Hdr = dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: uint32(ttl)}
 	rr2.AAAA = net.ParseIP(ipv6)
 	m.Answer = []dns.RR{rr1, rr2}
 	w.WriteMsg(m)
