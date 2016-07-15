2fe204940e66c57cd745d642d896e195f138e7b0
Faraz Fallahi <fffaraz@gmail.com>
2016-07-15 10:47:32 -0500

    args

--- microdns.go
+++ microdns.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	//"fmt"
 	"log"
 	"net"
 	"os"
@@ -11,7 +12,12 @@ import (
 	"github.com/miekg/dns"
 )
 
+var ipv4, ipv6 string
+
 func main() {
+	flag.StringVar(&ipv4, "ipv4", "127.0.0.1", "IPv4 Address")
+	flag.StringVar(&ipv6, "ipv6", "::1", "IPv6 Address")
+	flag.Parse()
 	dns.HandleFunc(".", handleRequest)
 	go func() {
 		srv := &dns.Server{Addr: ":53", Net: "udp"}
@@ -38,17 +44,17 @@ func main() {
 }
 
 func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
-	ip, _, _ := net.SplitHostPort(w.RemoteAddr().String())
-	fmt.Printf("%s\t%s\n", ip, r.Question[0].Name)
+	//ip, _, _ := net.SplitHostPort(w.RemoteAddr().String())
+	//fmt.Printf("%s\t%s\n", ip, r.Question[0].Name)
 	m := new(dns.Msg)
 	m.SetReply(r)
 	m.Authoritative = true
 	rr1 := new(dns.A)
 	rr1.Hdr = dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 86400}
-	rr1.A = net.ParseIP("127.0.0.1")
+	rr1.A = net.ParseIP(ipv4)
 	rr2 := new(dns.AAAA)
 	rr2.Hdr = dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 86400}
-	rr2.AAAA = net.ParseIP("::1")
+	rr2.AAAA = net.ParseIP(ipv6)
 	m.Answer = []dns.RR{rr1, rr2}
 	w.WriteMsg(m)
 }
