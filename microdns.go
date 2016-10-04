a24643d21be9f1cd6d14ed20463e312316f350ce
Faraz Fallahi <fffaraz@gmail.com>
2016-10-03 21:54:45 -0500

    1

--- microdns.go
+++ microdns.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"flag"
-	//"fmt"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"github.com/miekg/dns"
 )
 
-var ipv4, ipv6 string
+var ipv4, ipv6, conf string
 var ttl int
+var mapv4 map[string]string
+var mapv6 map[string]string
 
 func main() {
 	flag.StringVar(&ipv4, "ipv4", "127.0.0.1", "IPv4 Address")
 	flag.StringVar(&ipv6, "ipv6", "::1", "IPv6 Address")
 	flag.IntVar(&ttl, "ttl", 86400, "Time to live")
+	flag.StringVar(&conf, "conf", "", "Config File")
 	flag.Parse()
+	file, err := os.Open(conf)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		defer file.Close()
+		mapv4 = make(map[string]string)
+		mapv6 = make(map[string]string)
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Println(line)
+			fields := strings.Split(line, "\t")
+			mapv4[fields[0]] = fields[1]
+			mapv6[fields[0]] = fields[2]
+		}
+	}
+	//fmt.Println("\n---\n")
 	dns.HandleFunc(".", handleRequest)
 	go func() {
 		srv := &dns.Server{Addr: ":53", Net: "udp"}
@@ -45,17 +67,23 @@ func main() {
 }
 
 func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
+	domain := r.Question[0].Name
 	//ip, _, _ := net.SplitHostPort(w.RemoteAddr().String())
-	//fmt.Printf("%s\t%s\n", ip, r.Question[0].Name)
+	//fmt.Printf("%s\t%s\n", ip, domain)
 	m := new(dns.Msg)
 	m.SetReply(r)
 	m.Authoritative = true
 	rr1 := new(dns.A)
-	rr1.Hdr = dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: uint32(ttl)}
-	rr1.A = net.ParseIP(ipv4)
+	rr1.Hdr = dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: uint32(ttl)}
 	rr2 := new(dns.AAAA)
-	rr2.Hdr = dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: uint32(ttl)}
-	rr2.AAAA = net.ParseIP(ipv6)
+	rr2.Hdr = dns.RR_Header{Name: domain, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: uint32(ttl)}
+	if val, ok := mapv4[domain]; ok {
+		rr1.A = net.ParseIP(val)
+		rr2.AAAA = net.ParseIP(mapv6[domain])
+	} else {
+		rr1.A = net.ParseIP(ipv4)
+		rr2.AAAA = net.ParseIP(ipv6)
+	}
 	m.Answer = []dns.RR{rr1, rr2}
 	w.WriteMsg(m)
 }
